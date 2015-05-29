f50048929d5052a0efd68a4086e9578fb8ffaf35
Samuel Padgett <spadgett@redhat.com>
2015-05-29 16:13:31 -0400

    Update to osapi/v1beta3

--- fixtures/deploymentConfig.js
+++ fixtures/deploymentConfig.js
@@ -1,118 +1,117 @@
 window.EXAMPLE_DEP_CONFIG = {
-  "kind": "DeploymentConfig",
-  "apiVersion": "v1beta1",
   "metadata": {
-    "name": "frontend",
-    "namespace": "test",
-    "selfLink": "/osapi/v1beta1/deploymentConfigs/frontend?namespace=test",
-    "uid": "3626ef31-e208-11e4-bdc4-54ee75107c12",
-    "resourceVersion": "146",
-    "creationTimestamp": "2015-04-13T18:09:22Z",
+    "name": "database",
+    "namespace": "project1",
+    "selfLink": "/osapi/v1beta3/namespaces/project1/deploymentconfigs/database",
+    "uid": "01e61193-0620-11e5-82b1-0aa865aec90d",
+    "resourceVersion": "263",
+    "creationTimestamp": "2015-05-29T16:30:24Z",
     "labels": {
       "template": "application-template-stibuild"
     }
   },
-  "triggers": [
-    {
-      "type": "ImageChange",
-      "imageChangeParams": {
-        "automatic": true,
-        "containerNames": [
-          "ruby-helloworld"
-        ],
-        "from": {
-          "kind": "ImageRepository",
-          "name": "origin-ruby-sample"
-        },
-        "tag": "latest",
-        "lastTriggeredImage": "172.30.73.77:5000/test/origin-ruby-sample:7d7d7af5551062c36e680a81bfbd6598c9934b41139d9ed6c8ef13a64b9ffe72"
-      }
-    },
-    {
-      "type": "ConfigChange"
-    }    
-  ],
-  "template": {
+  "spec": {
     "strategy": {
-      "type": "Recreate"
-    },
-    "controllerTemplate": {
-      "replicas": 1,
-      "replicaSelector": {
-        "name": "frontend"
-      },
-      "podTemplate": {
-        "desiredState": {
-          "manifest": {
-            "version": "v1beta2",
-            "id": "",
-            "volumes": null,
-            "containers": [
+      "type": "Recreate",
+      "recreateParams": {
+        "pre": {
+          "failurePolicy": "Abort",
+          "execNewPod": {
+            "command": [
+              "/bin/true"
+            ],
+            "env": [
               {
-                "name": "ruby-helloworld",
-                "image": "172.30.73.77:5000/test/origin-ruby-sample:7d7d7af5551062c36e680a81bfbd6598c9934b41139d9ed6c8ef13a64b9ffe72",
-                "ports": [
-                  {
-                    "containerPort": 8080,
-                    "protocol": "TCP"
-                  }
-                ],
-                "env": [
-                  {
-                    "name": "ADMIN_USERNAME",
-                    "key": "ADMIN_USERNAME",
-                    "value": "admin4OS"
-                  },
-                  {
-                    "name": "ADMIN_PASSWORD",
-                    "key": "ADMIN_PASSWORD",
-                    "value": "GVuiK2je"
-                  },
-                  {
-                    "name": "MYSQL_USER",
-                    "key": "MYSQL_USER",
-                    "value": "userTJW"
-                  },
-                  {
-                    "name": "MYSQL_PASSWORD",
-                    "key": "MYSQL_PASSWORD",
-                    "value": "HOtpCU67"
-                  },
-                  {
-                    "name": "MYSQL_DATABASE",
-                    "key": "MYSQL_DATABASE",
-                    "value": "root"
-                  }
-                ],
-                "resources": {},
-                "terminationMessagePath": "/dev/termination-log",
-                "imagePullPolicy": "PullIfNotPresent",
-                "capabilities": {}
+                "name": "CUSTOM_VAR1",
+                "value": "custom_value1"
               }
             ],
-            "restartPolicy": {
-              "always": {}
-            },
-            "dnsPolicy": "ClusterFirst"
+            "containerName": "ruby-helloworld-database"
           }
         },
-        "labels": {
-          "name": "frontend",
-          "template": "application-template-stibuild"
+        "post": {
+          "failurePolicy": "Ignore",
+          "execNewPod": {
+            "command": [
+              "/bin/false"
+            ],
+            "env": [
+              {
+                "name": "CUSTOM_VAR2",
+                "value": "custom_value2"
+              }
+            ],
+            "containerName": "ruby-helloworld-database"
+          }
         }
+      },
+      "resources": {}
+    },
+    "triggers": [
+      {
+        "type": "ConfigChange"
+      }
+    ],
+    "replicas": 1,
+    "selector": {
+      "name": "database"
+    },
+    "template": {
+      "metadata": {
+        "creationTimestamp": null,
+        "labels": {
+          "name": "database"
+        }
+      },
+      "spec": {
+        "containers": [
+          {
+            "name": "ruby-helloworld-database",
+            "image": "openshift/mysql-55-centos7:latest",
+            "ports": [
+              {
+                "containerPort": 3306,
+                "protocol": "TCP"
+              }
+            ],
+            "env": [
+              {
+                "name": "MYSQL_USER",
+                "value": "userG4X"
+              },
+              {
+                "name": "MYSQL_PASSWORD",
+                "value": "20ipMAqW"
+              },
+              {
+                "name": "MYSQL_DATABASE",
+                "value": "root"
+              }
+            ],
+            "resources": {},
+            "terminationMessagePath": "/dev/termination-log",
+            "imagePullPolicy": "Always",
+            "capabilities": {},
+            "securityContext": {
+              "capabilities": {},
+              "privileged": false
+            }
+          }
+        ],
+        "restartPolicy": "Always",
+        "dnsPolicy": "ClusterFirst",
+        "serviceAccount": ""
       }
     }
   },
-  "latestVersion": 1,
-  "details": {
-    "causes": [
-      {
-        "type": "ImageChange",
-        "imageTrigger": {
-          "repositoryName": "172.30.73.77:5000/test/origin-ruby-sample:7d7d7af5551062c36e680a81bfbd6598c9934b41139d9ed6c8ef13a64b9ffe72",
-          "tag": "latest"
+  "status": {
+    "latestVersion": 1,
+    "details": {
+      "causes": [
+        {
+          "type": "ConfigChange"
         }
-      }
-    ]
+      ]
+    }
   }
 };
--- object-describer.js
+++ object-describer.js
@@ -5,14 +5,14 @@ try { angular.module("openshiftUI") } catch(e) { angular.module("openshiftUI", [
 angular.module("openshiftUI").requires.push("kubernetesUI");
 
 angular.module('openshiftUI')
-.filter('imageRepoReference', function(){
-  return function(objectRef, tag){
-    tag = tag || "latest";
-    var ns = objectRef.namespace || "";
-    ns = ns == "" ? ns : ns + "/";
-    var ref = ns + objectRef.name;
-    ref += " [" + tag + "]";
-    return ref;
+.filter('imageObjectRef', function(){
+  return function(objectRef, /* optional */ nsIfUnspecified){
+    var ns = objectRef.namespace || nsIfUnspecified;
+    if (ns && objectRef.kind !== "DockerImage") {
+      return ns + '/' + objectRef.name;
+    }
+
+    return objectRef.name;
   };
 })
 .run(function(KubernetesObjectDescriber) {
